7b66e21cb263cee61b3bc8bc719f46bde0261c7c
TylizRv <caosavenger@gmail.com>
2023-01-03 01:50:58 -0400

    Ejercicio del Reloj 4,5,6

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import ContactoListaComponente from './components/container/contacto_lista';
+// import ContactoListaComponente from './components/container/contacto_lista';
+import Clock from './components/pure/clock';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ContactoListaComponente></ContactoListaComponente>
+        {/* <ContactoListaComponente></ContactoListaComponente> */}
+        <Clock></Clock>
       </header>
     </div>
   );
