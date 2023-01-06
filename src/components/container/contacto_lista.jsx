36e351afb4b18f06aeb6e71f5334e69dd4590b82
TylizRv <caosavenger@gmail.com>
2023-01-06 15:22:15 -0400

    Ejercicios sesiones 7, 8 y 9

--- src/components/container/contacto_lista.jsx
+++ src/components/container/contacto_lista.jsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ContactoComponent from '../pure/contacto';
 import { Contacto } from '../../models/contacto.class';
-
+import ContactoForm from '../pure/forms/contactoForm';
 
 const ContactoListaComponente = () => {
 
-    const defaultContacto = new Contacto('Juan', 'Perez', 'perez@correo.com', true);
+	const defaultContacto = new Contacto('Juan', 'Perez', 'perez@correo.com', true);
 
-    return (
-        <div>
-            <ContactoComponent contacto={ defaultContacto }></ContactoComponent>
-        </div>
-    );
+	const [lstContacto, setLstContacto] = useState([defaultContacto]);
+
+	function addContact(contact) {
+		const tmpLstContact = [...lstContacto, contact];
+		setLstContacto(tmpLstContact);
+	}
+
+	function removeContact(contact) {
+		const tmpLstContact = [...lstContacto];
+		const index = tmpLstContact.indexOf(contact);
+		tmpLstContact.splice(index, 1);
+		setLstContacto(tmpLstContact);
+	}
+
+	function updateContact(contact) {
+		const tmpLstContact = [...lstContacto];
+		const index = tmpLstContact.indexOf(contact);
+		tmpLstContact[index].Conectado = !tmpLstContact[index].Conectado;
+		setLstContacto(tmpLstContact);
+	}
+
+	return (
+		<div className='row'>
+			<ContactoForm add={ addContact }></ContactoForm>
+			<div className='col-sm-12 col-md-6'>
+				<fieldset className='fieldset'>
+					<legend className='fieldset__legend'>Lista de contactos</legend>
+					{
+						lstContacto.map((contacto, index) => {
+							return (<ContactoComponent key={ index } contacto={ contacto } update={ updateContact } remove={ removeContact }></ContactoComponent>);
+						})
+					}
+				</fieldset>
+			</div>
+		</div>
+	);
 };
 
 export default ContactoListaComponente;
--- src/components/pure/contacto.jsx
+++ src/components/pure/contacto.jsx
@@ -1,32 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Contacto } from '../../models/contacto.class';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const ContactoComponent = ({ contacto, update, remove }) => {
 
-const ContactoComponent = ({ contacto }) => {
+	const estadoContacto = () => {
+		let icon = null;
+		if (contacto.Conectado) {
+			icon = (
+				<div className='contact__icon' onClick={ () => update(contacto) }>
+					<FontAwesomeIcon icon='fa-solid fa-user' className='icono-conectado'/> <span>En Línea</span>
+				</div>
+			);
+		} else {
+			icon = (
+				<div className='contact__icon contact__icon--desconectado' onClick={ () => update(contacto) }>
+					<FontAwesomeIcon icon='fa-regular fa-user' className='icono-desconectado'/> <span>Desconectado</span>
+				</div>
+			);
+		}
+		return icon;
+	}
 
-    const [conectado, setConectado] = useState(contacto.Conectado);
-
-    const changeState = () => {
-        contacto.Conectado = !contacto.Conectado;
-        setConectado(!conectado);
-    }
-
-    return (
-        <div>
-            <h1>Nombre: { contacto.Nombre } { contacto.Apellido }</h1>
-            <h2>Email: { contacto.Email }</h2>
-            <h4>{ contacto.Conectado ? 'Contacto En Línea' : 'Contacto No Disponible' }</h4>
-            <button onClick={ changeState }>
-                Cambiar Estado
-            </button>
-        </div>
-    );
+	return (
+		<div className='col-sm-12 contact'>
+			{ estadoContacto() }
+			<div className='contact__info'>
+				<span className='contact__label'>
+					<b>Nombre:</b> { contacto.Nombre } { contacto.Apellido }
+				</span>
+				<span className='contact__label'>
+					<b>Email:</b> { contacto.Email }
+				</span>
+			</div>
+			<div className='contact__action'>
+				<button onClick={ () => remove(contacto) } className='btn btn-danger'>
+					<FontAwesomeIcon icon='fa-solid fa-trash-can' />
+				</button>
+			</div>
+		</div>
+	);
 };
 
 
 ContactoComponent.propTypes = {
-    contacto: PropTypes.instanceOf(Contacto),
+	contacto: PropTypes.instanceOf(Contacto).isRequired,
+	update: PropTypes.func.isRequired,
+	remove: PropTypes.func.isRequired,
 };
 
 
